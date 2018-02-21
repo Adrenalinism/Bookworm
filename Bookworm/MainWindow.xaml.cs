a046207e50d9e06b14074a410e77293621a58da7
Marko <marko_bandzo@hotmail.com>
2018-02-22 12:47:43 +1300

    almost there

--- Bookworm/MainWindow.xaml.cs
+++ Bookworm/MainWindow.xaml.cs
@@ -21,10 +21,32 @@ namespace Bookworm
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] lines = File.ReadLines("C:\\Users\\Marko\\Downloads\\english-words-master\\english-words-master\\words_alpha.txt").ToArray();
+        string[] all_words = File.ReadLines("C:\\Users\\Marko\\Downloads\\english-words-master\\english-words-master\\words_alpha.txt").ToArray();
+        char[] available_letters = { 's', 'a', 'm', 'e', 'd', 'e', 'e', 'i', 'd', 's', 'r', 'p', 'g', 'a', 'z', 'w' };
+        const int wordlength_limit = 5;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < all_words.Length; i++) // go through dictionary
+            {
+                char[] current_word_letters = all_words[i].ToCharArray(); // place each word in a char array as its being inspected
+                if (current_word_letters.Length >= wordlength_limit) // only check relatively long words for increased speed
+                {
+                    for (int j = 0; j < current_word_letters.Length; j++) // go thorugh characters of each word
+                    {
+                        if (available_letters.Contains(current_word_letters[j])) // if letter of current word is in the available letters
+                        {
+                            
+                        }
+                        else
+                        {
+                            j = 20;
+                        }
+                    }
+                }
+            }
             
         }
     }
