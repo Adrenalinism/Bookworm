473c876078d2777698084f42c74fd057214bd539
Marko <marko_bandzo@hotmail.com>
2018-02-22 14:26:38 +1300

    prototype fully working, cleanup is all that is left

--- Bookworm/MainWindow.xaml.cs
+++ Bookworm/MainWindow.xaml.cs
@@ -22,31 +22,50 @@ namespace Bookworm
     public partial class MainWindow : Window
     {
         string[] all_words = File.ReadLines("C:\\Users\\Marko\\Downloads\\english-words-master\\english-words-master\\words_alpha.txt").ToArray();
-        char[] available_letters = { 's', 'a', 'm', 'e', 'd', 'e', 'e', 'i', 'd', 's', 'r', 'p', 'g', 'a', 'z', 'w' };
+        char[] available_letters = new char[16];
+        char[] original_letters = { 's', 'x', 'm', 'x', 'i', 'e', 'e', 'i', 'd', 's', 'r', 'p', 'g', 'x', 'z', 'w' };
         const int wordlength_limit = 5;
+
+        
+
+        string largest_word = "";
 
         public MainWindow()
         {
             InitializeComponent();
+            
 
             for (int i = 0; i < all_words.Length; i++) // go through dictionary
             {
+                Array.Copy(original_letters, available_letters, 16);
+
+                int letters_matched = 0;
+                int exit = 0;
                 char[] current_word_letters = all_words[i].ToCharArray(); // place each word in a char array as its being inspected
+
                 if (current_word_letters.Length >= wordlength_limit) // only check relatively long words for increased speed
                 {
                     for (int j = 0; j < current_word_letters.Length; j++) // go thorugh characters of each word
                     {
-                        if (available_letters.Contains(current_word_letters[j])) // if letter of current word is in the available letters
+                        if (available_letters.Contains(current_word_letters[j]) && exit == 0) // if letter of current word is in the available letters
                         {
-                            
+                            letters_matched++; // increase the count of letters of current word that are matched
+                            available_letters[Array.IndexOf(available_letters, current_word_letters[j])] = '$'; // prevent letter from being used again in the loop
+                            //available_letters = available_letters.Select(x => x.Replace("one", "zero")).ToArray();
+
+                            if (letters_matched == current_word_letters.Length && current_word_letters.Length > largest_word.Length) // if all letters of word exist in available letters and if this is word is the biggest yet, make it the biggest
+                            {
+                                largest_word = all_words[i];
+                            }
                         }
                         else
                         {
-                            j = 20;
+                            exit = 1;
                         }
                     }
                 }
             }
+            label1.Content = largest_word;
             
         }
     }
